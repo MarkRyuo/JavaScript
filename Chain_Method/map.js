032c9d380b7f2329c5e64112bce395c42f760a09
Riyuo <arcx@Riyuos-MacBook-Air.local>
2026-03-05 18:39:33 +0800

    Subject: Update

---
+++ Chain_Method/map.js
@@ -0,0 +1,45 @@
+
+
+const products = [
+  { name: "Laptop", price: 55000, color: "Silver" },
+  { name: "Smartphone", price: 25000, color: "Black" },
+  { name: "Headphones", price: 3500, color: "White" },
+  { name: "Keyboard", price: 1200, color: "Black" },
+  { name: "Mouse", price: 800, color: "Gray" },
+  { name: "Monitor", price: 9000, color: "Black" },
+  { name: "Tablet", price: 18000, color: "Gold" },
+  { name: "Speaker", price: 2200, color: "Blue" },
+  { name: "Webcam", price: 1500, color: "Black" },
+  { name: "Microphone", price: 2700, color: "Red" },
+  { name: "Power Bank", price: 1300, color: "White" },
+  { name: "Smartwatch", price: 7000, color: "Space Gray" }
+];
+
+
+const discounts = products.map((product) => {
+    if(product.price > 0.5)
+    return {
+        ...product,
+        price: product.price * 0.5,
+    };
+});
+
+//console.log(discounts);
+
+const newProduct = products.map((product) => {
+    let category ;
+    if(product.price >= 20000){
+        category = "High";
+    }else if(product.price >= 10000){
+        category = "Mid";
+    }else {
+        category = "Low";
+    }
+
+    return {
+        ...product,
+        category: product.category
+    }
+})
+
+console.log(newProduct)
